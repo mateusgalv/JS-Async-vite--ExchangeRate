d17a003b7bda79b0f6df6bb557e16c2269a1a2e2
Mateus Galvao <mateustgalvao@outlook.com>
2025-06-01 15:19:56 -0300

    1.1

--- src/utils/rates.js
+++ src/utils/rates.js
@@ -1,7 +1,5 @@
-const API_KEY = "12e6e2d0bd6f4b72fb7af04b";
-
 export const getRates = async (currency = "BRL") => {
-    const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`;
+    const URL = `https://v6.exchangerate-api.com/v6/12e6e2d0bd6f4b72fb7af04b/latest/${currency}`;
     console.log(URL);
     return fetch(URL)
     .then(response => response.json())
