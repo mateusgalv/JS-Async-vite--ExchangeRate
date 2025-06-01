2c897e5848263c56579d131a8e4792b358bca7d0
Mateus Galvao <mateustgalvao@outlook.com>
2025-05-31 22:05:12 -0300

    all functions avl

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import "./styles/style.css";
 import { getRates } from "./utils/rates.js";
 
-const table = document.getElementById('table');
+const dropdown = document.getElementById("currency-dropdown");
+const applyNewCurrency = document.getElementById("fetch");
+const tableContainer = document.getElementById("table-container");
+
+const setDropDownOptions = (currencies) => {
+    currencies.forEach(currency => {
+        const option = document.createElement("option");
+        
+        option.innerText = currency;
+        dropdown.appendChild(option);
+    });
+}
 
 const createTableRow = () => {
     const row = document.createElement("tr");
@@ -9,24 +20,57 @@ const createTableRow = () => {
     table.appendChild(row);
 }
 
-const createTableElement = (currency, rate=0) => {
+const createTableElement = (currency, rate) => {
     const currentRow = table.lastElementChild;
 
     const data = document.createElement("td");
-    data.innerText = currency;
-    data.classList.add("currency");
+    const currencyElement = document.createElement("div");
+    const rateElement = document.createElement("div");
+    
+    currencyElement.innerText = currency;
+    rateElement.innerText = rate;
+    
+    data.appendChild(currencyElement);
+    data.appendChild(rateElement);
     currentRow.appendChild(data);
 }
+
+const createTable = (rates, currencies) => {
+    const table = document.createElement("table");
+    table.id = "table";
+    tableContainer.appendChild(table);
+    
+    currencies.shift() // first element = selected currency
+    
+    currencies.forEach((currency, index) => {
+        if(index === 0 || index%3 === 0) {
+            createTableRow();
+        }
+        const currentRate = rates[`${currency}`].toFixed(2);
+        createTableElement(currency, currentRate);
+    });
+}
+
+const removeTable = () => {
+    const currentTable = document.getElementById("table");
+    currentTable.remove();
+}
+
+applyNewCurrency.addEventListener("click", async () => {
+    const rates = await getRates(dropdown.value);
+    const currencies = Object.keys(rates);
+
+    sessionStorage.setItem("rates", JSON.stringify(rates));
+    removeTable();
+    createTable(rates, currencies);
+});
 
 window.onload = async () => {
     const rates = await getRates();
     const currencies = Object.keys(rates);
 
-    currencies.forEach((currency, index) => {
-        if(index === 0 || index%3 === 0) {
-            createTableRow();
-        }
-
-        createTableElement(currency);
-    });
+    sessionStorage.setItem("rates", JSON.stringify(rates));
+    
+    setDropDownOptions(currencies);
+    createTable(rates, currencies);
 }
--- src/utils/rates.js
+++ src/utils/rates.js
@@ -1,6 +1,9 @@
 const API_KEY = "12e6e2d0bd6f4b72fb7af04b";
-const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
 
-export const getRates = () => fetch(URL)
+export const getRates = async (currency = "BRL") => {
+    const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`;
+    console.log(URL);
+    return fetch(URL)
     .then(response => response.json())
     .then(data => data.conversion_rates);
+}
