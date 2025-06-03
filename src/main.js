ac67ac41c979813140465fe7a6f211499fffd582
Mateus Galvão <110851306+mateusgalv@users.noreply.github.com>
2025-06-03 23:11:21 +0000

    feature/dark/mode

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { getRates } from "./utils/rates.js";
 const dropdown = document.getElementById("currency-dropdown");
 const applyNewCurrency = document.getElementById("fetch");
 const tableContainer = document.getElementById("table-container");
+const body = document.getElementById("main");
+const mode = document.getElementById("mode");
+
 
 const setDropDownOptions = (currencies) => {
     currencies.forEach(currency => {
@@ -22,14 +25,14 @@ const createTableRow = () => {
 
 const createTableElement = (currency, rate) => {
     const currentRow = table.lastElementChild;
-
+    
     const data = document.createElement("td");
     data.classList.add("table-data");
-
+    
     const currencyElement = document.createElement("div");
     currencyElement.classList.add("currency-element");
     currencyElement.innerText = currency;
-
+    
     const rateElement = document.createElement("div");
     rateElement.classList.add("rate-element");
     rateElement.innerText = rate;
@@ -63,16 +66,24 @@ const removeTable = () => {
 applyNewCurrency.addEventListener("click", async () => {
     const rates = await getRates(dropdown.value);
     const currencies = Object.keys(rates);
-
+    
     sessionStorage.setItem("rates", JSON.stringify(rates));
     removeTable();
     createTable(rates, currencies);
 });
 
+// body.classList.remove(class);
+mode.addEventListener('click', () => {
+    const currentMode = body.className;
+    body.className = "";
+
+    currentMode == "light" ? body.className = 'dark' : body.className = 'light';
+});
+
 window.onload = async () => {
     const rates = await getRates();
     const currencies = Object.keys(rates);
-
+    
     sessionStorage.setItem("rates", JSON.stringify(rates));
     
     setDropDownOptions(currencies);
