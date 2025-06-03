dc568eff01c6d7b72debd5f7261d1ef523f247c4
Mateus Galvão <110851306+mateusgalv@users.noreply.github.com>
2025-06-03 22:42:16 +0000

    test-commit

--- src/utils/rates.js
+++ src/utils/rates.js
@@ -1,7 +1,6 @@
 export const getRates = async (currency = "BRL") => {
     try {
         const URL = `https://v6.exchangerate-api.com/v6/12e6e2d0bd6f4b72fb7af04b/latest/${currency}`;
-        console.log(URL);
         return fetch(URL)
         .then(response => response.json())
         .then(data => data.conversion_rates);
