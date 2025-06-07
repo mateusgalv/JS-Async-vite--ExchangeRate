8d2f1774e6e2b156c853451b5aa8c6a0933bd925
Mateus Galvao <mateustgalvao@outlook.com>
2025-06-07 02:18:58 -0300

    long

--- src/main.js
+++ src/main.js
@@ -1,12 +1,146 @@
 import "./styles/style.css";
 import { getRates } from "./utils/rates.js";
 
-const dropdown = document.getElementById("currency-dropdown");
-const applyNewCurrency = document.getElementById("fetch");
-const tableContainer = document.getElementById("table-container");
 const body = document.getElementById("main");
-const modeSelector = document.getElementById("mode-selector");
+const dropdown = document.getElementById("currency-dropdown");
+const tableContainer = document.getElementById("table-container");
+const currencyContainer = document.getElementById("currency-container");
+const currency = document.getElementById("currency");
 
+const themeToggle = document.getElementById("theme-toggle");
+const favToggle = document.getElementById("fav-toggle");
+
+const applyBtn = document.getElementById("apply-btn");
+
+const userOptions = document.getElementsByClassName("user-options")[0];
+
+const createTableElement = () => {
+    const table = document.createElement("table");
+    table.id = "table";    
+    table.classList.add(currentTheme());
+    tableContainer.appendChild(table);
+
+    return table;
+}
+
+const createTableRowElement = () => {
+    const row = document.createElement("tr");
+    row.classList.add("row");
+    table.appendChild(row);
+
+    return row;
+}
+
+const createDiv = (className, value) => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add(className);
+    newDiv.innerText = value;
+
+    return newDiv;
+}
+
+const createTableDataElement = (currency, rate) => {
+    const currentRow = table.lastElementChild;
+    
+    const data = document.createElement("td");
+    data.classList.add("table-data", currentTheme());  
+    data.addEventListener("click", () => data.classList.toggle("favorite"));
+    
+    const currencyElement = createDiv("currency-element", currency);
+    const rateElement = createDiv("rate-element", rate);
+    
+    data.appendChild(currencyElement);
+    data.appendChild(rateElement);
+    currentRow.appendChild(data);
+
+    return data;
+}
+
+const currentTheme = () => body.className;
+
+const getTable = () => document.getElementById("table");
+
+const getFavorites = () => {
+    const collection = document.getElementsByClassName("favorite");
+    
+    return [...collection];
+}
+
+const removeTable = () => {
+    const table = getTable();
+    table.remove();
+}
+
+const toggleTheme = (element) => {
+    element.classList.remove("dark", "light");
+    currentTheme() === "dark" ? element.classList.add("dark") : element.classList.add("light");
+}
+
+const uncheckFav = () => favToggle.checked = false;
+
+const switchAllElementsTheme = () => {
+    const allTableDataElements = document.getElementsByTagName("td");
+    const allElements = [getTable(), currencyContainer, userOptions, tableContainer, ...allTableDataElements];
+
+    allElements.forEach((element) => {
+        toggleTheme(element);
+    })
+}
+
+const displayFavCurrencies = () => {
+    const favorites = getFavorites();
+
+    removeTable();
+    createFavTable(favorites);
+}
+
+const displayAllCurrencies = () => {
+    const rates = JSON.parse(sessionStorage.getItem("rates"));
+    const currencies = Object.keys(rates);
+    const favorites = getFavorites();
+
+    removeTable();
+    createFullTable(rates, currencies);
+    const allElements = [...document.getElementsByTagName("td")];
+    favorites.forEach((favorite) => {
+        allElements.forEach((element) => {
+            if(element.firstChild.innerText === favorite.firstChild.innerText) {
+                element.classList.add("favorite");
+            }
+        });
+    });
+}
+
+applyBtn.addEventListener("click", async () => {
+    const rates = await getRates(dropdown.value);
+    const currencies = Object.keys(rates);
+    changeCurrencyOnText(currencies[0]);
+    
+    sessionStorage.setItem("rates", JSON.stringify(rates));
+    removeTable();
+    createFullTable(rates, currencies);
+    uncheckFav();
+});
+
+themeToggle.addEventListener("click", () => {
+    const currentTheme = body.className;
+    body.className = "";
+
+    currentTheme == "light" ? body.className = 'dark' : body.className = 'light';
+    switchAllElementsTheme();
+});
+
+favToggle.addEventListener("change", (e) => {
+    if(e.target.checked) {
+        displayFavCurrencies();
+    } else {
+        displayAllCurrencies();
+    }
+});
+
+const changeCurrencyOnText = (newCurrency) => {
+    currency.innerText = `Valores de 1.00 ${newCurrency}`;
+}
 
 const setDropDownOptions = (currencies) => {
     currencies.forEach(currency => {
@@ -17,94 +151,40 @@ const setDropDownOptions = (currencies) => {
     });
 }
 
-const createTableRow = () => {
-    const row = document.createElement("tr");
-    row.classList.add("row");
-    table.appendChild(row);
+const createFavTable = (favorites) => {
+    const table = createTableElement();
+    
+    favorites.forEach((favorite, index) => {
+        if(index === 0 || index%3 === 0) {
+            createTableRowElement();
+        }
+        const currentRow = table.lastElementChild;
+        currentRow.appendChild(favorite);
+    });
 }
 
-const createTableElement = (currency, rate) => {
-    const currentRow = table.lastElementChild;
-    
-    const data = document.createElement("td");
-    data.classList.add("table-data");
-    
-    const currencyElement = document.createElement("div");
-    currencyElement.classList.add("currency-element");
-    currencyElement.innerText = currency;
-    
-    const rateElement = document.createElement("div");
-    rateElement.classList.add("rate-element");
-    rateElement.innerText = rate;
-    
-    data.appendChild(currencyElement);
-    data.appendChild(rateElement);
-    currentRow.appendChild(data);
-}
+const createFullTable = (rates, currencies) => {
+    createTableElement();
 
-const createTable = (rates, currencies) => {
-    const table = document.createElement("table");
-    table.id = "table";
-    tableContainer.appendChild(table);
-    
     currencies.shift() // first element = selected currency
     
     currencies.forEach((currency, index) => {
         if(index === 0 || index%3 === 0) {
-            createTableRow();
+            createTableRowElement();
         }
         const currentRate = rates[`${currency}`].toFixed(4);
-        createTableElement(currency, currentRate);
+        createTableDataElement(currency, currentRate);
     });
 }
-
-const removeTable = () => {
-    const currentTable = document.getElementById("table");
-    currentTable.remove();
-}
-
-const checkCurrMode = () => body.className;
-
-const switchElementMode = (element) => {
-    const currElmentClasses = element.classList()
-    const currMode = checkCurrMode();
-
-    const newClasses = currElmentClasses.map((elementClass) => {
-        if (elementClass !== currMode) {
-            return elementClass;
-        }
-    });
-
-    currMode === 'dark' ? newClasses.push('light') : newClasses.push('dark');
-
-    newClasses.forEach((newClass) => {
-        element.classList.add(newClass);
-    });
-}
-
-
-applyNewCurrency.addEventListener("click", async () => {
-    const rates = await getRates(dropdown.value);
-    const currencies = Object.keys(rates);
-    
-    sessionStorage.setItem("rates", JSON.stringify(rates));
-    removeTable();
-    createTable(rates, currencies);
-});
-
-modeSelector.addEventListener('click', () => {
-    const currentMode = body.className;
-    body.className = "";
-
-    currentMode == "light" ? body.className = 'dark' : body.className = 'light';
-});
 
 window.onload = async () => {
     const rates = await getRates();
     const currencies = Object.keys(rates);
+    const mainCurrency = currencies[0];
     
     sessionStorage.setItem("rates", JSON.stringify(rates));
     
+    changeCurrencyOnText(mainCurrency);
     setDropDownOptions(currencies);
-    createTable(rates, currencies);
+    createFullTable(rates, currencies);
 }
